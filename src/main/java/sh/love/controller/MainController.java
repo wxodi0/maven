87a8e5f65919542dbcc99893a0014ec72381fc1b
wxodi0 <teayang06@gmail.com>
2023-12-26 16:10:46 +0900

    init

--- src/main/java/sh/love/controller/MainController.java
+++ src/main/java/sh/love/controller/MainController.java
@@ -3,14 +3,14 @@ package sh.love.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import sh.love.jpa.domain.Main;
 import sh.love.service.MainService;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class MainController {
 
     @Autowired
@@ -24,5 +24,18 @@ public class MainController {
         model.addAttribute("mainTitle", "Hello");
         return "/main";
     }
+    @PostMapping("/")
+    public String main2(Model model) {
+        return "post";
+    }
 
+    @PutMapping("/")
+    public String main3(Model model) {
+        return "put";
+    }
+
+    @DeleteMapping("/")
+    public String main4(Model model) {
+        return "delete";
+    }
 }
